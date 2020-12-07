863ceeeec46dc0ed8807b134667dd58c2feac95b
Travis Lange <travislange12@gmail.com>
2020-12-07 15:47:39 -0500

    added support to show that things break in embedded packages

--- Assets/OtherScripts/OtherTestScript.cs
+++ Assets/OtherScripts/OtherTestScript.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using TMPro;
+using Newtonsoft.Json;
+using UnityEngine.UI;
 
 public class OtherTestScript : MonoBehaviour
 {
--- Packages/com.consulo.bug/Scripts/TestScript.cs
+++ Packages/com.consulo.bug/Scripts/TestScript.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Newtonsoft.Json;
 
 public class TestScript : MonoBehaviour
 {
